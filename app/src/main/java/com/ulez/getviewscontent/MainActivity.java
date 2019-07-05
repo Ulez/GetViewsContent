5bdbdbc9cfcfca771600c1ae87c917f3815c60cf
Ulez <1104128773@qq.com>
2019-07-05 15:17:40 +0800

    .....

--- app/src/main/java/com/ulez/getviewscontent/MainActivity.java
+++ app/src/main/java/com/ulez/getviewscontent/MainActivity.java
@@ -1,17 +1,31 @@
 package com.ulez.getviewscontent;
 
 import android.content.Intent;
+import android.preference.PreferenceActivity;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.bt).setOnClickListener(this);
         startService(new Intent(this, MyAccessibilityService.class));
 //        Log.e("lcy","startService");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt:
+                Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                startActivity(intent);
+                break;
+        }
     }
 }
