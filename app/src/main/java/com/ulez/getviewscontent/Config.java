15e42ad3144651321829d144aa01c7d50efd4838
Ulez <1104128773@qq.com>
2019-08-19 19:22:46 +0800

    海尔优家获取识别文本；

--- app/src/main/java/com/ulez/getviewscontent/Config.java
+++ app/src/main/java/com/ulez/getviewscontent/Config.java
@@ -1,6 +1,6 @@
 package com.ulez.getviewscontent;
 
 public class Config {
-    public static final String packageName="com.miui.voiceassist";
+    public static final String packageName="com.haier.uhome.uplus";
     public static final String TextName="android.widget.TextView";
 }
--- app/src/main/java/com/ulez/getviewscontent/MyAccessibilityService.java
+++ app/src/main/java/com/ulez/getviewscontent/MyAccessibilityService.java
@@ -1,26 +1,33 @@
 package com.ulez.getviewscontent;
 
 import android.accessibilityservice.AccessibilityService;
+import android.graphics.Rect;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MyAccessibilityService extends AccessibilityService {
-    private static final String TAG = "MyAccessibilityService";
+    private static final String TAG = "accessibilityservice";
     private int mDebugDepth;
     private AccessibilityNodeInfo mNodeInfo;
     private long lastTime = 0;
     private static final String pattern = "\"(.*)\" # ";
+    private int index = 0;
+    private int lastIndex = 0;
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
 //        Log.e(TAG, "onAccessibilityEvent==" + event.getClassName());
         mDebugDepth = 0;
         mNodeInfo = event.getSource();
+        index = 0;
         printAllViews(mNodeInfo);
 //        Log.e(TAG, String.format(
 //                "onAccessibilityEvent: type = [ %s ], class = [ %s ], package = [ %s ], time = [ %s ], text = [ %s ]",
@@ -60,27 +67,22 @@ public class MyAccessibilityService extends AccessibilityService {
         for (int i = 0; i < mDebugDepth; i++) {
             log += ".";
         }
-        if (mDebugDepth >= 6 && Config.packageName.equals(mNodeInfo.getPackageName().toString()) && Config.TextName.equals(mNodeInfo.getClassName().toString())) {
-            CharSequence text = mNodeInfo.getText();
-            if (System.currentTimeMillis() - lastTime > 800) {
-//                log += "(" + text + " <-- " + mNodeInfo.getViewIdResourceName() + ")";
-                String result = text.toString();
-                // 创建 Pattern 对象
-                Pattern r = Pattern.compile(pattern);
-                // 现在创建 matcher 对象
-                Matcher m = r.matcher(result);
-                if (m.find()) {
-                    System.out.println("Found value: " + m.group(1));
-                    result = m.group(1);
-                } else {
-                    result = "";
-                }
-                Toast.makeText(MyAccessibilityService.this, result, Toast.LENGTH_SHORT).show();
-                Log.e(TAG, "asr=" + result);
-                lastTime = System.currentTimeMillis();
-            }
+        String text = "";
+        try {
+            text = mNodeInfo.getText().toString();
+        } catch (Exception e) {
         }
-        if (mNodeInfo.getChildCount() < 1) return;
+        if (mDebugDepth == 9 && !TextUtils.isEmpty(text) && !"  ".equals(text)) {
+            Rect outBounds = new Rect();
+            mNodeInfo.getBoundsInScreen(outBounds);
+            if (outBounds.right==840){
+                Log.i(TAG, index + ":text=" + text);
+            }
+            index++;
+        }
+        if (mNodeInfo.getChildCount() < 1) {
+            return;
+        }
         mDebugDepth++;
         for (int i = 0; i < mNodeInfo.getChildCount(); i++) {
             printAllViews(mNodeInfo.getChild(i));
